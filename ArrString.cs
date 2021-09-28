dc34312fdfffd2a610cc9cb848ad77a7e6f44e15
ACER <nargizer@code.edu.az>
2021-09-28 10:44:15 +0400

    28.09

---
+++ ArrString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add text:");
+            string txt = Console.ReadLine();
+            Split(txt);
+
+        }
+
+        static void Split(string sentence1)
+        {
+            string[] result1 = sentence1.Split(' ');
+
+            string result2 = string.Empty;
+            for(int i=result1.Length-1; i>=0; i--)
+            {
+                result2 += result1[i]+" ";
+            }
+
+            string result = string.Empty;
+            for (int i = result2.Length - 1; i >= 0; i--)
+            {
+                result += result2[i];
+            }
+            Console.WriteLine($"Reversed: {result}");
+        }
+    }
+}
